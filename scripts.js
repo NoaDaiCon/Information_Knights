1f74c2acb494602b989c9229f5743fa8968b1695
DeeJayHicks <connor.itouch@gmail.com>
2013-04-02 09:39:20 -0400

    0.0.5

--- scripts.js
+++ scripts.js
@@ -1,29 +1,20 @@
-function check()
-{
-	if($("#ans").val()=="Hello")
-	{
-		$("#nextPanel").panel( "open" );
-		document.getElementById("nextPanel.rightWrong").innerHTML="Correct!";
-		$( "#mypanel" ).trigger( "updatelayout" );
-	}
-	else
-	{
-		document.getElementById("q").html=$("#ans").val();
-	}
-
-}
 
 function createQuestion()
 {
-	var first==Math.floor(Math.random()*101);
-	var sec==Math.floor(Math.random()*101);
+	var first=Math.floor(Math.random()*101);
+	var sec=Math.floor(Math.random()*101);
 	var type=Math.floor(Math.random()*5);
 	var ans;
 	var ques;
 	
+		document.getElementById("firstVar").innerHtml(first);
+		document.getElementById("secondVar").innerHtml(second);
+		document.getElementById("typeVar").innerHtml(type);
+	
 	if(type==0)
 	{
 		ans = first + sec;
-		ques = first + "+" + sec + "= ?";
+		ques = first + "+" + sec + "= ?";		
 	}
 }
+
