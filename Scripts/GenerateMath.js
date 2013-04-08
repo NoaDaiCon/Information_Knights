e682c57b4ed2103ffd0d33aea620aaae64e9a04d
DeeJayHicks <connor.itouch@gmail.com>
2013-04-08 08:42:13 -0400

    0.1.0

---
+++ Scripts/GenerateMath.js
@@ -0,0 +1,73 @@
+
+var first;
+var sec;
+var type
+var ans;
+
+function generate()
+{
+	first=Math.floor(Math.random()*101);
+	sec=Math.floor(Math.random()*101);
+	type=Math.floor(Math.random()*4);	
+	
+	if(type==0)
+	{
+		ans=first+sec;
+	}
+	
+	else if(type==1)
+	{
+		ans=first-sec;
+	}
+	
+	else if(type==2)
+	{
+		ans=first*sec;
+	}
+	
+	else if(type==3)
+	{
+		ans=first/sec;
+	}
+	
+	else
+	{
+	}
+}
+
+
+function getQuestion()
+{
+	
+	if(type==0)
+	{
+		return(first + "+" + sec + "=");
+	}
+	
+	else if(type==1)
+	{
+		return(first + "-" + sec + "=");
+	}
+	
+	else if(type==2)
+	{
+		return(first + "*" + sec + "=");
+	}
+	
+	else if(type==3)
+	{
+		return(first + "/" + sec + "=");
+	}
+	
+	else
+	{
+		return("Error");
+	}
+	
+	
+}
+
+function getAnswer()
+{
+	return(ans);
+}
